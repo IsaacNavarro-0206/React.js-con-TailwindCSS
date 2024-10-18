abd5866690436f5cdca2df93b5b89cfb2737248b
Isaac Navarro <isaacnavarro2000@gmail.com>
2024-10-17 21:34:19 -0500

    feat: creación e implementación del componente card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import Card from "../../components/Card";
+
 function Home() {
-  return <div>Home</div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card
+            title={item.title}
+            category={item.category.name}
+            price={item.price}
+            image={item.images}
+            key={item.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
